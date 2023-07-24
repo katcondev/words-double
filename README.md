6dd9cfe0d045300b9d241bc8287dac0f6f60696e
katcontrerasdev <kat.contreras@gmail.com>
2023-07-24 13:13:11 -0700

    moved location

---
+++ README.md
@@ -0,0 +1,4 @@
+# double_words
+
+A new Flutter project.
+
---
+++ android/app/src/main/kotlin/com/example/double_words/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.double_words
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
